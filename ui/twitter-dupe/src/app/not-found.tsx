bdcdd51d15e229c5006370195d49b9577165cfdf
Judd Rollins <hjrolli2@ncsu.edu>
2024-01-18 18:07:44 -0500

    fixed route push

--- ui/twitter-dupe/src/app/not-found.tsx
+++ ui/twitter-dupe/src/app/not-found.tsx
@@ -1,26 +1,39 @@
-'use client'
-import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import isRouteValid from '../../lib/routes';
- 
-export default function NotFound() {
+"use client";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+import Image from "next/image";
+import isRouteValid from "../../lib/routes";
+import notFound from "../../public/assets/funny-error-404-background-design_1167-219.png";
 
+export default function NotFound() {
   const pathname = usePathname();
   const validRoute = isRouteValid(pathname);
   const router = useRouter();
 
-  console.log(validRoute)
+  useEffect(() => {
+    if (validRoute) {
+      router.push(pathname);
+    }
+  }, [pathname, router, validRoute]);
 
 
-
-  
-  
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Image src={notFound} alt="404 Not Found" width={500} height={400} />
+      <h1 style={{ fontSize: "3rem", margin: "20px 0" }}>
+        Oops! Page Not Found
+      </h1>
+      <Link
+        style={{
+          fontSize: "1.5rem",
+          color: "#0070f3",
+          textDecoration: "underline",
+        }}
+        href="/"
+      >
+        GO HOME
+      </Link>
     </div>
-  )
+  );
 }
